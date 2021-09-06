5182b45248a471de6c3828faf4b29c00bfe4dc1e
Muthukumaran-G <muthukumaran.gnanavinayagam@syncfusion.com>
2021-09-06 12:26:27 +0530

    Sample committed.

---
+++ FetchFromAPI/FetchFromAPI/Model.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+
+namespace FetchFromAPI
+{
+    public class Model
+    {
+        public string country { get; set; }
+        public List<string> web_pages { get; set; }
+        public string alpha_two_code { get; set; }
+        public string name { get; set; }
+
+        [JsonProperty("state-province")]
+        public object StateProvince { get; set; }
+        public List<string> domains { get; set; }
+
+        [JsonIgnore]
+        public Command TapCommand { get; set; }
+
+        public Model()
+        {
+            TapCommand = new Command(OnTapped);
+        }
+
+        private async void OnTapped(object obj)
+        {
+            var canOpen = await Launcher.TryOpenAsync(new System.Uri(obj.ToString()));
+            if (!canOpen)
+                await App.Current.MainPage.DisplayAlert("Oops!", "Cannot open browser!", "OK");
+        }
+    }
+}
---
+++ FetchFromAPI/FetchFromAPI/ViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+
+namespace FetchFromAPI
+{
+    public class ViewModel
+    {
+        public List<Model> Items { get; set; }
+        public ViewModel()
+        {
+            var taskString = new HttpClient().GetStringAsync(new Uri("http://universities.hipolabs.com/search?country=United+States"));
+            var modelList = JsonConvert.DeserializeObject<List<Model>>(taskString.Result);
+            Items = modelList.ToList();
+        }
+    }
+}
